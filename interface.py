2cc816328eb639894e4931c140ab36ed9068f0ab
Денис <nisya.iv@gmail.com>
2025-02-26 20:23:05 +0300

    ПОЧТИ работающая пятая задача

--- interface.py
+++ interface.py
@@ -2,20 +2,21 @@ import pygame
 
 
 class Button:
-    def __init__(self, screen, button_cor, button_size):
+    def __init__(self, screen, button_cor, button_size, text=""):
         self.screen = screen
         self.button_x = button_cor[0]
         self.button_y = button_cor[1]
         self.button_width = button_size[0]
         self.button_height = button_size[1]
-        self.color = None
         self.font = pygame.font.Font(None, 30)
         self.theme = "light"
         self.count = 0
         self.lmb = False
+        self.result = False
+        self.text = text
 
     def update(self):
-        result = False
+        self.result = False
         mx, my = pygame.mouse.get_pos()
         theme_button = pygame.Surface((self.button_width, self.button_height))
         theme_button.fill((200, 200, 200))
@@ -25,13 +26,57 @@ class Button:
             if pygame.mouse.get_pressed()[0] and not self.lmb:
                 self.count = (self.count + 1) % 2
                 self.theme = ["light", "dark"][self.count]
-                result = True
+                self.result = True
                 self.lmb = True
 
         if not pygame.mouse.get_pressed()[0]:
             self.lmb = False
-        theme_text = self.font.render(self.theme, True, "black")
-        theme_button.blit(theme_text, (7, 6))
+        if self.text:
+            theme_text = self.font.render(self.text, True, "black")
+        else:
+            theme_text = self.font.render(self.theme, True, "black")
+        theme_button.blit(theme_text, (7, (self.button_height // 2) - 10))
         self.screen.blit(theme_button, (self.button_x, self.button_y))
 
-        return result
+
+class FindLine:
+    def __init__(self, screen, line_cor, line_size):
+        self.screen = screen
+        self.line_x = line_cor[0]
+        self.line_y = line_cor[1]
+        self.line_width = line_size[0]
+        self.line_height = line_size[1]
+        self.lmb = False
+        self.input_active = False
+        self.text = ""
+        self.font = pygame.font.Font(None, 30)
+
+    def update(self, events):
+        mx, my = pygame.mouse.get_pos()
+        find_line = pygame.Surface((self.line_width, self.line_height))
+        find_line.fill((255, 255, 255))
+
+        if pygame.mouse.get_pressed()[0] and not self.lmb:
+            self.lmb = True
+            if (self.line_x <= mx <= self.line_x + self.line_width and
+                    self.line_y <= my <= self.line_y + self.line_height):
+                self.input_active = True
+            else:
+                self.input_active = False
+
+        if not pygame.mouse.get_pressed()[0]:
+            self.lmb = False
+
+        self.input_text(events)
+        line_text = self.font.render(self.text, True, "black")
+        find_line.blit(line_text, (7, 6))
+        self.screen.blit(find_line, (self.line_x, self.line_y))
+
+    def input_text(self, events):
+        if self.input_active:
+            for event in events:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_BACKSPACE and self.text:
+                        self.text = self.text[:-1]
+                    elif event.unicode.isprintable():
+                        self.text += event.unicode
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sys
 import pygame
 import requests
 import os
-from interface import Button
+from interface import Button, FindLine
 
 
 def create_responce(api, parampampam):
@@ -12,10 +12,23 @@ def create_responce(api, parampampam):
             f.write(response.content)
 
 
+def find_place_cord(place):
+    server = "http://geocode-maps.yandex.ru/1.x/?"
+    API = "8013b162-6b42-4997-9691-77b7074026e0"
+    geocoder = place
+    request = f"{server}apikey={API}&geocode={geocoder}&format=json"
+    response = requests.get(request)
+    if response:
+        pos = response.json()["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"]
+        return pos
+    else:
+        return False
+
+
 if __name__ == '__main__':
     pygame.init()
 
-    screen = pygame.display.set_mode((750, 450))
+    screen = pygame.display.set_mode((1000, 600))
 
     info = pygame.display.Info()
     center = [info.current_w / 2, info.current_h / 2]
@@ -24,14 +37,16 @@ if __name__ == '__main__':
     upper_center = [info.current_w / 2, 0]
     lower_center = [info.current_w / 2, info.current_h]
 
-    theme_button = Button(screen, (center[0] * 2 - 70 - 20, 30 / 2 + 10), (70, 30))
+    theme_button = Button(screen, (right_center[0] - 90, 10), (70, 30))
+    findline = FindLine(screen, (center[0] - 325, 20), (400, 35))
+    find_button = Button(screen, (center[0] + 80, 15), (80, 45), text="Найти")
 
     api_server = "https://static-maps.yandex.ru/v1"
     lon = "28.98513"
     lat = "41.036943"
     delta1 = "0.0029"
     delta2 = "0.0029"
-    apikey = "81b63ec7-b5bf-4e94-97fd-5645a56b1305"
+    apikey = "f3a0fe3a-b07e-4840-a1da-06f18b2ddf13"
     scale_coefficient = 0.25
 
     params = {
@@ -45,18 +60,15 @@ if __name__ == '__main__':
     create_responce(api_server, params)
 
     map_screen = pygame.image.load("map.png")
-    screen.blit(map_screen, (0, 0))
+    screen.blit(map_screen, (center[0] - 325, center[1] - 225))
     pygame.display.flip()
     while True:
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 os.remove("map.png")
                 pygame.quit()
                 sys.exit()
-
-            if theme_button.update():
-                params["theme"] = theme_button.theme
-                create_responce(api_server, params)
 
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_PAGEUP:
@@ -80,6 +92,23 @@ if __name__ == '__main__':
                     params["ll"] = ",".join([lon, lat])
                     create_responce(api_server, params)
 
+        theme_button.update()
+        findline.update(events)
+        find_button.update()
+
+        if theme_button.result:
+            params["theme"] = theme_button.theme
+            create_responce(api_server, params)
+
+        if find_button.result:
+            pos = find_place_cord(findline.text)
+            if pos:
+                lon, lat = pos.split()
+                params["pt"] = f"{lon},{lat},pm2rdm"
+                params["ll"] = f"{lon},{lat}"
+                create_responce(api_server, params)
+            else:
+                print("Не удалось найти координаты для введенного адреса.")
         map_screen = pygame.image.load("map.png")
-        screen.blit(map_screen, (0, 0))
+        screen.blit(map_screen, (center[0] - 325, center[1] - 225))
         pygame.display.flip()
